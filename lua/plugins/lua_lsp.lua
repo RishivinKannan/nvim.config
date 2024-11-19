380ef75ad76e374520e340dfcfd482ad109b5ed9
Rishivin <rishivin@atatus.com>
2024-11-19 23:35:09 +0530

    fix: tab size 2 to 4

---
+++ lua/plugins/lua_lsp.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            format = {
+                enable = true,
+                defaultConfig = {
+                    indent_style = "space",
+                    indent_size = "4",
+                },
+            },
+        },
+    },
+}
