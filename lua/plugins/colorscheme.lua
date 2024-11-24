ea6bf98f71579aae6639f154b710cb2da4eb415f
Rishivin <rishivin@atatus.com>
2024-11-24 20:22:16 +0530

    update: colorscheme onedark with bg transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,14 @@
 return {
-  { "shaunsingh/nord.nvim" },
-  {
-    "projekt0n/github-nvim-theme",
-    config = function()
-      vim.cmd("colorscheme github_dark_tritanopia")
-    end,
-  },
-  {
-    "navarasu/onedark.nvim",
-    config = function()
-      require("onedark").setup({
-        style = "deep", -- You can choose "dark", "darker", "cool", etc.
-      })
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "night" },
-  },
+    { "shaunsingh/nord.nvim" },
+    {
+        "projekt0n/github-nvim-theme",
+        config = function()
+            require("github-theme").setup({})
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        opts = { style = "night" },
+    },
 }
