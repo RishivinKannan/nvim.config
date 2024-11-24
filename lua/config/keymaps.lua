feff2d387150b701760ae020c4f583fef86960b0
Rishivin <rishivin@atatus.com>
2024-11-24 20:25:35 +0530

    feat: new keymaps
    Don't yank when x in normal mode
    Scroll and center in search and scroll
    Ctrl + q to quit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,5 +6,29 @@ local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Map <leader>tv to open a terminal in a vertical split
-keymap("t", "<C-v>", [[<C-\><C-n>:lua Snacks.terminal.toggle()<CR>]], opts)
-keymap("n", "<M-/>", [[<C-\><C-n>:lua Snacks.terminal.toggle("bash")<CR>]], opts)
+keymap("t", "<C-v>", [[<cmd>:lua Snacks.terminal.toggle()<CR>]], opts)
+keymap("n", "<M-/>", [[<cmd>:lua Snacks.terminal.toggle("bash")<CR>]], opts)
+
+-- save file without auto-formatting
+vim.keymap.set("n", "<leader>sN", "<cmd>noautocmd w <CR>", opts)
+
+-- Keep last yanked when pasting
+vim.keymap.set("v", "p", '"_dP', opts)
+
+-- Move text up and down
+vim.keymap.set("v", "<A-j>", ":m .+1<CR>==", opts)
+vim.keymap.set("v", "<A-k>", ":m .-2<CR>==", opts)
+
+-- quit file
+vim.keymap.set("n", "<C-q>", "<cmd> q <CR>", opts)
+
+-- delete single character without copying into register
+vim.keymap.set("n", "x", '"_x', opts)
+
+-- Vertical scroll and center
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+
+-- Find and center
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
